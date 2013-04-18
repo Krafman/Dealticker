ce079291533362d995b669690de3e547f2f4a554
Adir Krafman <adirkrafman@gmail.com>
2013-04-18 13:45:38 -0400

    Form and CSS and sidenav

--- public_html/contact/contactform.php
+++ public_html/contact/contactform.php
@@ -7,12 +7,12 @@
     http://www.html-form-guide.com/contact-form/php-contact-form-tutorial.html
 */
 require_once("./include/fgcontactform.php");
-require_once("./include/captcha-creator.php");
+//require_once("./include/captcha-creator.php");
 
 $formproc = new FGContactForm();
-$captcha = new FGCaptchaCreator('scaptcha');
+/*$captcha = new FGCaptchaCreator('scaptcha');
 
-$formproc->EnableCaptcha($captcha);
+//$formproc->EnableCaptcha($captcha); */
 
 //1. Add your email address here.
 //You can add more than one receipients.
@@ -38,8 +38,10 @@ if(isset($_POST['submitted']))
 <head>
       <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
       <title>Contact us</title>
+      <link rel="STYLESHEET" type="text/css" href="../css/bootstrap.css" />
       <link rel="STYLESHEET" type="text/css" href="contact.css" />
-      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+    
+    <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
       <script type='text/javascript' src='scripts/fg_captcha_validator.js'></script>
 </head>
 <body>
@@ -47,47 +49,47 @@ if(isset($_POST['submitted']))
 <!-- Form Code Start -->
 <form id='contactus' action='<?php echo $formproc->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
 <fieldset >
-<legend>Contact us</legend>
+<legend>Fill out your information below:</legend>
 
 <input type='hidden' name='submitted' id='submitted' value='1'/>
 <input type='hidden' name='<?php echo $formproc->GetFormIDInputName(); ?>' value='<?php echo $formproc->GetFormIDInputValue(); ?>'/>
-<input type='text'  class='spmhidip' name='<?php echo $formproc->GetSpamTrapInputName(); ?>' />
+<input type='hidden'  class='spmhidip' name='<?php echo $formproc->GetSpamTrapInputName(); ?>' />
 
-<div class='short_explanation'>* required fields</div>
+<div class='label label-important'>* required fields</div>
 
 <div><span class='error'><?php echo $formproc->GetErrorMessage(); ?></span></div>
-<div class='container'>
+<div class='contr'>
     <label for='name' >Your Full Name*: </label><br/>
     <input type='text' name='name' id='name' value='<?php echo $formproc->SafeDisplay('name') ?>' maxlength="50" /><br/>
     <span id='contactus_name_errorloc' class='error'></span>
 </div>
-<div class='container'>
+<div class='contr'>
     <label for='email' >Email Address*:</label><br/>
     <input type='text' name='email' id='email' value='<?php echo $formproc->SafeDisplay('email') ?>' maxlength="50" /><br/>
     <span id='contactus_email_errorloc' class='error'></span>
 </div>
-<div class='container'>
+<div class='contr'>
     <label for='phone' >Phone Number*:</label><br/>
     <input type='text' name='phone' id='phone' value='<?php echo $formproc->SafeDisplay('phone') ?>' maxlength="50" /><br/>
     <span id='contactus_phone_errorloc' class='error'></span>
 </div>
-<div class='container'>
+<div class='contr'>
     <label for='message' >Message:</label><br/>
     <span id='contactus_message_errorloc' class='error'></span>
     <textarea rows="10" cols="50" name='message' id='message'><?php echo $formproc->SafeDisplay('message') ?></textarea>
 </div>
-<div class='container'>
+<!-- <div class='contr'>
     <div><img alt='Captcha image' src='show-captcha.php?rand=1' id='scaptcha_img' /></div>
     <label for='scaptcha' >Enter the code above here:</label>
     <input type='text' name='scaptcha' id='scaptcha' maxlength="10" /><br/>
     <span id='contactus_scaptcha_errorloc' class='error'></span>
     <div class='short_explanation'>Can't read the image?
     <a href='javascript: refresh_captcha_img();'>Click here to refresh</a>.</div>
-</div>
+</div> -->
 
 
-<div class='container'>
-    <input type='submit' name='Submit' value='Submit' />
+<div class='contr'>
+    <button class="btn btn-primary" type='submit' name='Submit' value='Submit' /> Submit </button>
 </div>
 
 </fieldset>
@@ -112,24 +114,24 @@ Uses the excellent form validation script from JavaScript-coder.com-->
     frmvalidator.addValidation("message","maxlen=2048","The message is too long!(more than 2KB!)");
 
 
-    frmvalidator.addValidation("scaptcha","req","Please enter the code in the image above");
+    //frmvalidator.addValidation("scaptcha","req","Please enter the code in the image above");
 
-    document.forms['contactus'].scaptcha.validator
-      = new FG_CaptchaValidator(document.forms['contactus'].scaptcha,
-                    document.images['scaptcha_img']);
+    // document.forms['contactus'].scaptcha.validator
+    //   = new FG_CaptchaValidator(document.forms['contactus'].scaptcha,
+    //                 document.images['scaptcha_img']);
 
-    function SCaptcha_Validate()
-    {
-        return document.forms['contactus'].scaptcha.validator.validate();
-    }
+    // function SCaptcha_Validate()
+    // {
+    //     return document.forms['contactus'].scaptcha.validator.validate();
+    // }
 
-    frmvalidator.setAddnlValidationFunction("SCaptcha_Validate");
+    // frmvalidator.setAddnlValidationFunction("SCaptcha_Validate");
 
-    function refresh_captcha_img()
-    {
-        var img = document.images['scaptcha_img'];
-        img.src = img.src.substring(0,img.src.lastIndexOf("?")) + "?rand="+Math.random()*1000;
-    }
+    // function refresh_captcha_img()
+    // {
+    //     var img = document.images['scaptcha_img'];
+    //     img.src = img.src.substring(0,img.src.lastIndexOf("?")) + "?rand="+Math.random()*1000;
+    // }
 
 // ]]>
 </script>
